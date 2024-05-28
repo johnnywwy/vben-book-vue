09cf40f7b8031610b09ed102eb5b8cb7ec61d99c
wuwenyu <johnnywwy@gmail.com>
2024-05-28 18:31:57 +0800

    feat: 引入缺失组件

--- src/components/Form/src/components/FormItem.vue
+++ src/components/Form/src/components/FormItem.vue
@@ -1,23 +1,23 @@
 <script lang="tsx">
-  import { type Recordable, type Nullable } from '@vben/types';
-  import type { PropType, Ref } from 'vue';
-  import { computed, defineComponent, toRefs, unref } from 'vue';
-  import type { FormActionType, FormProps, FormSchema } from '../types/form';
-  import type { ValidationRule } from 'ant-design-vue/lib/form/Form';
-  import type { TableActionType } from '/@/components/Table';
-  import { Col, Divider, Form } from 'ant-design-vue';
-  import { componentMap } from '../componentMap';
-  import { BasicHelp } from '/@/components/Basic';
-  import { isBoolean, isFunction, isNull } from '/@/utils/is';
-  import { getSlot } from '/@/utils/helper/tsxHelper';
+  import { type Recordable, type Nullable } from '@vben/types'
+  import type { PropType, Ref } from 'vue'
+  import { computed, defineComponent, toRefs, unref } from 'vue'
+  import type { FormActionType, FormProps, FormSchema } from '../types/form'
+  import type { ValidationRule } from 'ant-design-vue/lib/form/Form'
+  import type { TableActionType } from '/@/components/Table'
+  import { Col, Divider, Form } from 'ant-design-vue'
+  import { componentMap } from '../componentMap'
+  import { BasicHelp } from '/@/components/Basic'
+  import { isBoolean, isFunction, isNull } from '/@/utils/is'
+  import { getSlot } from '/@/utils/helper/tsxHelper'
   import {
     createPlaceholderMessage,
     NO_AUTO_LINK_COMPONENTS,
     setComponentRuleType,
-  } from '../helper';
-  import { cloneDeep, upperFirst } from 'lodash-es';
-  import { useItemLabelWidth } from '../hooks/useLabelWidth';
-  import { useI18n } from '/@/hooks/web/useI18n';
+  } from '../helper'
+  import { cloneDeep, upperFirst } from 'lodash-es'
+  import { useItemLabelWidth } from '../hooks/useLabelWidth'
+  import { useI18n } from '/@/hooks/web/useI18n'
 
   export default defineComponent({
     name: 'BasicFormItem',
@@ -54,18 +54,18 @@
       },
     },
     setup(props, { slots }) {
-      const { t } = useI18n();
+      const { t } = useI18n()
 
       const { schema, formProps } = toRefs(props) as {
-        schema: Ref<FormSchema>;
-        formProps: Ref<FormProps>;
-      };
+        schema: Ref<FormSchema>
+        formProps: Ref<FormProps>
+      }
 
-      const itemLabelWidthProp = useItemLabelWidth(schema, formProps);
+      const itemLabelWidthProp = useItemLabelWidth(schema, formProps)
 
       const getValues = computed(() => {
-        const { allDefaultValues, formModel, schema } = props;
-        const { mergeDynamicData } = props.formProps;
+        const { allDefaultValues, formModel, schema } = props
+        const { mergeDynamicData } = props.formProps
         return {
           field: schema.field,
           model: formModel,
@@ -75,14 +75,14 @@
             ...formModel,
           } as Recordable<any>,
           schema: schema,
-        };
-      });
+        }
+      })
 
       const getComponentsProps = computed(() => {
-        const { schema, tableAction, formModel, formActionType } = props;
-        let { componentProps = {} } = schema;
+        const { schema, tableAction, formModel, formActionType } = props
+        let { componentProps = {} } = schema
         if (isFunction(componentProps)) {
-          componentProps = componentProps({ schema, tableAction, formModel, formActionType }) ?? {};
+          componentProps = componentProps({ schema, tableAction, formModel, formActionType }) ?? {}
         }
         if (schema.component === 'Divider') {
           componentProps = Object.assign(
@@ -92,51 +92,51 @@
               plain: true,
             },
             componentProps,
-          );
+          )
         }
-        return componentProps as Recordable<any>;
-      });
+        return componentProps as Recordable<any>
+      })
 
       const getDisable = computed(() => {
-        const { disabled: globDisabled } = props.formProps;
-        const { dynamicDisabled } = props.schema;
-        const { disabled: itemDisabled = false } = unref(getComponentsProps);
-        let disabled = !!globDisabled || itemDisabled;
+        const { disabled: globDisabled } = props.formProps
+        const { dynamicDisabled } = props.schema
+        const { disabled: itemDisabled = false } = unref(getComponentsProps)
+        let disabled = !!globDisabled || itemDisabled
         if (isBoolean(dynamicDisabled)) {
-          disabled = dynamicDisabled;
+          disabled = dynamicDisabled
         }
         if (isFunction(dynamicDisabled)) {
-          disabled = dynamicDisabled(unref(getValues));
+          disabled = dynamicDisabled(unref(getValues))
         }
-        return disabled;
-      });
+        return disabled
+      })
 
       function getShow(): { isShow: boolean; isIfShow: boolean } {
-        const { show, ifShow } = props.schema;
-        const { showAdvancedButton } = props.formProps;
+        const { show, ifShow } = props.schema
+        const { showAdvancedButton } = props.formProps
         const itemIsAdvanced = showAdvancedButton
           ? isBoolean(props.isAdvanced)
             ? props.isAdvanced
             : true
-          : true;
+          : true
 
-        let isShow = true;
-        let isIfShow = true;
+        let isShow = true
+        let isIfShow = true
 
         if (isBoolean(show)) {
-          isShow = show;
+          isShow = show
         }
         if (isBoolean(ifShow)) {
-          isIfShow = ifShow;
+          isIfShow = ifShow
         }
         if (isFunction(show)) {
-          isShow = show(unref(getValues));
+          isShow = show(unref(getValues))
         }
         if (isFunction(ifShow)) {
-          isIfShow = ifShow(unref(getValues));
+          isIfShow = ifShow(unref(getValues))
         }
-        isShow = isShow && itemIsAdvanced;
-        return { isShow, isIfShow };
+        isShow = isShow && itemIsAdvanced
+        return { isShow, isIfShow }
       }
 
       function handleRules(): ValidationRule[] {
@@ -147,31 +147,31 @@
           label,
           dynamicRules,
           required,
-        } = props.schema;
+        } = props.schema
 
         if (isFunction(dynamicRules)) {
-          return dynamicRules(unref(getValues)) as ValidationRule[];
+          return dynamicRules(unref(getValues)) as ValidationRule[]
         }
 
-        let rules: ValidationRule[] = cloneDeep(defRules) as ValidationRule[];
-        const { rulesMessageJoinLabel: globalRulesMessageJoinLabel } = props.formProps;
+        let rules: ValidationRule[] = cloneDeep(defRules) as ValidationRule[]
+        const { rulesMessageJoinLabel: globalRulesMessageJoinLabel } = props.formProps
 
         const joinLabel = Reflect.has(props.schema, 'rulesMessageJoinLabel')
           ? rulesMessageJoinLabel
-          : globalRulesMessageJoinLabel;
-        const defaultMsg = createPlaceholderMessage(component) + `${joinLabel ? label : ''}`;
+          : globalRulesMessageJoinLabel
+        const defaultMsg = createPlaceholderMessage(component) + `${joinLabel ? label : ''}`
 
         function validator(rule: any, value: any) {
-          const msg = rule.message || defaultMsg;
+          const msg = rule.message || defaultMsg
           if (value === undefined || isNull(value)) {
             // 空值
-            return Promise.reject(msg);
+            return Promise.reject(msg)
           } else if (Array.isArray(value) && value.length === 0) {
             // 数组类型
-            return Promise.reject(msg);
+            return Promise.reject(msg)
           } else if (typeof value === 'string' && value.trim() === '') {
             // 空字符串
-            return Promise.reject(msg);
+            return Promise.reject(msg)
           } else if (
             typeof value === 'object' &&
             Reflect.has(value, 'checked') &&
@@ -182,12 +182,12 @@
             value.halfChecked.length === 0
           ) {
             // 非关联选择的tree组件
-            return Promise.reject(msg);
+            return Promise.reject(msg)
           }
-          return Promise.resolve();
+          return Promise.resolve()
         }
 
-        const getRequired = isFunction(required) ? required(unref(getValues)) : required;
+        const getRequired = isFunction(required) ? required(unref(getValues)) : required
 
         /*
          * 1、若设置了required属性，又没有其他的rules，就创建一个验证规则；
@@ -196,49 +196,49 @@
          */
         if (getRequired) {
           if (!rules || rules.length === 0) {
-            rules = [{ required: getRequired, validator }];
+            rules = [{ required: getRequired, validator }]
           } else {
-            const requiredIndex: number = rules.findIndex((rule) => Reflect.has(rule, 'required'));
+            const requiredIndex: number = rules.findIndex((rule) => Reflect.has(rule, 'required'))
 
             if (requiredIndex === -1) {
-              rules.push({ required: getRequired, validator });
+              rules.push({ required: getRequired, validator })
             }
           }
         }
 
         const requiredRuleIndex: number = rules.findIndex(
           (rule) => Reflect.has(rule, 'required') && !Reflect.has(rule, 'validator'),
-        );
+        )
 
         if (requiredRuleIndex !== -1) {
-          const rule = rules[requiredRuleIndex];
-          const { isShow } = getShow();
+          const rule = rules[requiredRuleIndex]
+          const { isShow } = getShow()
           if (!isShow) {
-            rule.required = false;
+            rule.required = false
           }
           if (component) {
             if (!Reflect.has(rule, 'type')) {
-              rule.type = component === 'InputNumber' ? 'number' : 'string';
+              rule.type = component === 'InputNumber' ? 'number' : 'string'
             }
 
-            rule.message = rule.message || defaultMsg;
+            rule.message = rule.message || defaultMsg
 
             if (component.includes('Input') || component.includes('Textarea')) {
-              rule.whitespace = true;
+              rule.whitespace = true
             }
-            const valueFormat = unref(getComponentsProps)?.valueFormat;
-            setComponentRuleType(rule, component, valueFormat);
+            const valueFormat = unref(getComponentsProps)?.valueFormat
+            setComponentRuleType(rule, component, valueFormat)
           }
         }
 
         // Maximum input length rule check
-        const characterInx = rules.findIndex((val) => val.max);
+        const characterInx = rules.findIndex((val) => val.max)
         if (characterInx !== -1 && !rules[characterInx].validator) {
           rules[characterInx].message =
             rules[characterInx].message ||
-            t('component.form.maxTip', [rules[characterInx].max] as Recordable<any>);
+            t('component.form.maxTip', [rules[characterInx].max] as Recordable<any>)
         }
-        return rules;
+        return rules
       }
 
       function renderComponent() {
@@ -248,109 +248,107 @@
           field,
           changeEvent = 'change',
           valueField,
-        } = props.schema;
+        } = props.schema
 
-        const isCheck = component && ['Switch', 'Checkbox'].includes(component);
+        const isCheck = component && ['Switch', 'Checkbox'].includes(component)
 
-        const eventKey = `on${upperFirst(changeEvent)}`;
+        const eventKey = `on${upperFirst(changeEvent)}`
 
         const on = {
           [eventKey]: (...args: Nullable<Recordable<any>>[]) => {
-            const [e] = args;
+            const [e] = args
             if (propsData[eventKey]) {
-              propsData[eventKey](...args);
+              propsData[eventKey](...args)
             }
-            const target = e ? e.target : null;
-            const value = target ? (isCheck ? target.checked : target.value) : e;
-            props.setFormModel(field, value, props.schema);
+            const target = e ? e.target : null
+            const value = target ? (isCheck ? target.checked : target.value) : e
+            props.setFormModel(field, value, props.schema)
           },
-        };
-        const Comp = componentMap.get(component) as ReturnType<typeof defineComponent>;
+        }
+        const Comp = componentMap.get(component) as ReturnType<typeof defineComponent>
 
-        const { autoSetPlaceHolder, size } = props.formProps;
+        const { autoSetPlaceHolder, size } = props.formProps
         const propsData: Recordable<any> = {
           allowClear: true,
           getPopupContainer: (trigger: Element) => trigger.parentNode,
           size,
           ...unref(getComponentsProps),
           disabled: unref(getDisable),
-        };
+        }
 
-        const isCreatePlaceholder = !propsData.disabled && autoSetPlaceHolder;
+        const isCreatePlaceholder = !propsData.disabled && autoSetPlaceHolder
         // RangePicker place is an array
         if (isCreatePlaceholder && component !== 'RangePicker' && component) {
           propsData.placeholder =
-            unref(getComponentsProps)?.placeholder || createPlaceholderMessage(component);
+            unref(getComponentsProps)?.placeholder || createPlaceholderMessage(component)
         }
-        propsData.codeField = field;
-        propsData.formValues = unref(getValues);
+        propsData.codeField = field
+        propsData.formValues = unref(getValues)
 
         const bindValue: Recordable<any> = {
           [valueField || (isCheck ? 'checked' : 'value')]: props.formModel[field],
-        };
+        }
 
         const compAttr: Recordable<any> = {
           ...propsData,
           ...on,
           ...bindValue,
-        };
+        }
 
         if (!renderComponentContent) {
-          return <Comp {...compAttr} />;
+          return <Comp {...compAttr} />
         }
         const compSlot = isFunction(renderComponentContent)
           ? { ...renderComponentContent(unref(getValues)) }
           : {
               default: () => renderComponentContent,
-            };
-        return <Comp {...compAttr}>{compSlot}</Comp>;
+            }
+        return <Comp {...compAttr}>{compSlot}</Comp>
       }
 
       function renderLabelHelpMessage() {
-        const { label, helpMessage, helpComponentProps, subLabel } = props.schema;
+        const { label, helpMessage, helpComponentProps, subLabel } = props.schema
         const renderLabel = subLabel ? (
           <span>
             {label} <span class="text-secondary">{subLabel}</span>
           </span>
         ) : (
           label
-        );
-        const getHelpMessage = isFunction(helpMessage)
-          ? helpMessage(unref(getValues))
-          : helpMessage;
+        )
+        const getHelpMessage = isFunction(helpMessage) ? helpMessage(unref(getValues)) : helpMessage
         if (!getHelpMessage || (Array.isArray(getHelpMessage) && getHelpMessage.length === 0)) {
-          return renderLabel;
+          return renderLabel
         }
         return (
           <span>
             {renderLabel}
             <BasicHelp placement="top" class="mx-1" text={getHelpMessage} {...helpComponentProps} />
           </span>
-        );
+        )
       }
 
       function renderItem() {
-        const { itemProps, slot, render, field, suffix, component } = props.schema;
-        const { labelCol, wrapperCol } = unref(itemLabelWidthProp);
-        const { colon } = props.formProps;
+        const { itemProps, slot, render, field, suffix, component } = props.schema
+        const { labelCol, wrapperCol } = unref(itemLabelWidthProp)
+        const { colon } = props.formProps
 
         if (component === 'Divider') {
           return (
             <Col span={24}>
               <Divider {...unref(getComponentsProps)}>{renderLabelHelpMessage()}</Divider>
             </Col>
-          );
+          )
         } else {
           const getContent = () => {
             return slot
               ? getSlot(slots, slot, unref(getValues))
               : render
               ? render(unref(getValues))
-              : renderComponent();
-          };
+              : renderComponent()
+          }
 
-          const showSuffix = !!suffix;
-          const getSuffix = isFunction(suffix) ? suffix(unref(getValues)) : suffix;
+          const showSuffix = !!suffix
+          const getSuffix = isFunction(suffix) ? suffix(unref(getValues)) : suffix
 
           // TODO 自定义组件验证会出现问题，因此这里框架默认将自定义组件设置手动触发验证，如果其他组件还有此问题请手动设置autoLink=false
           if (NO_AUTO_LINK_COMPONENTS.includes(component)) {
@@ -358,7 +356,7 @@
               (props.schema.itemProps! = {
                 autoLink: false,
                 ...props.schema.itemProps,
-              });
+              })
           }
 
           return (
@@ -377,28 +375,28 @@
                 {showSuffix && <span class="suffix">{getSuffix}</span>}
               </div>
             </Form.Item>
-          );
+          )
         }
       }
 
       return () => {
-        const { colProps = {}, colSlot, renderColContent, component } = props.schema;
+        const { colProps = {}, colSlot, renderColContent, component } = props.schema
         if (!componentMap.has(component)) {
-          return null;
+          return null
         }
 
-        const { baseColProps = {} } = props.formProps;
-        const realColProps = { ...baseColProps, ...colProps };
-        const { isIfShow, isShow } = getShow();
-        const values = unref(getValues);
+        const { baseColProps = {} } = props.formProps
+        const realColProps = { ...baseColProps, ...colProps }
+        const { isIfShow, isShow } = getShow()
+        const values = unref(getValues)
 
         const getContent = () => {
           return colSlot
             ? getSlot(slots, colSlot, values)
             : renderColContent
             ? renderColContent(values)
-            : renderItem();
-        };
+            : renderItem()
+        }
 
         return (
           isIfShow && (
@@ -406,8 +404,8 @@
               {getContent()}
             </Col>
           )
-        );
-      };
+        )
+      }
     },
-  });
+  })
 </script>
